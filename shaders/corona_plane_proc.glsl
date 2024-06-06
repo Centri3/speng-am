cbfaf59322db8d83c71515e93378617073af5ef3
Centri3 <114838443+Centri3@users.noreply.github.com>
2024-06-06 12:04:12 -0500

    messing with the coronae a bit

--- shaders/corona_plane_proc.glsl
+++ shaders/corona_plane_proc.glsl
@@ -56,20 +56,38 @@ void main(void)
     // Xanii would be proud
     bool is_blue = _color.b > (_color.r + _color.g);
 
-    vec2 uv0 = in_tex_coord * 2.0 - 1.0;
+    vec2 uv0 = in_tex_coord * 2.0f - 1.0f;
     vec2 uv = uv0 * shape.ray_curvature * 0.05;
 	vec2 uvn = normalize(uv);
 
     float r0 = sqrt(dot(uv0, uv0));
     float r = sqrt(dot(uv, uv));
-    float x = dot(uvn, vec2(0.5, 0.0)) - eye.anim_time;
-    float y = dot(uvn, vec2(0.0, 0.5)) - eye.anim_time;
+    float x = dot(uvn, vec2(0.5f, 0.0f)) - eye.anim_time;
+    float y = dot(uvn, vec2(0.0f, 0.5f)) - eye.anim_time;
+
+    vec2 dist = noise_fbm_vec2(
+        vec3(
+            (r + x) / 10.0f,
+            (r + y) / 10.0f,
+            0.0f
+        ),
+        NoiseParams(6.0f, 6.0f, 0.2f, 0.0f, 0.0001, 0.13, 0.85)
+    );
+
+    uv0 = (in_tex_coord + dist * 0.01f) * 2.0f - 1.0f;
+    uv = uv0 * shape.ray_curvature * 0.05;
+	uvn = normalize(uv);
+
+    r0 = sqrt(dot(uv0, uv0));
+    r = sqrt(dot(uv, uv));
+    x = dot(uvn, vec2(0.5f, 0.0f)) - eye.anim_time;
+    y = dot(uvn, vec2(0.0f, 0.5f)) - eye.anim_time;
 
     float rays = noise_fbm_float(
         vec3(
-            (r + x) / 60.0 * shape.ray_density,
-            (r + y) / 60.0 * shape.ray_density,
-            0.0
+            (r + x) / 60.0f * shape.ray_density,
+            (r + y) / 60.0f * shape.ray_density,
+            0.0f
         ),
         NoiseParams(2.6f, 8.0f, 0.2f, 0.0f, 0.0001, 0.1, 0.55)
     );
@@ -91,7 +109,47 @@ void main(void)
     color += vec3(0.3 * (_color.r + _color.g + _color.b));
     color *= saturate(0.74f - eucl_dist);
 
-    uv  = uv0 * 36.0f * 0.05;
+    uv = uv0 * 36.0f * 0.05;
+	uvn = normalize(uv);
+
+    r0 = sqrt(dot(uv0, uv0));
+    r = sqrt(dot(uv, uv));
+    x = dot(uvn, vec2(0.5, 0.0)) - eye.anim_time / 5.0f;
+    y = dot(uvn, vec2(0.0, 0.5)) - eye.anim_time / 5.0f;
+
+    float prominences = noise_fbm_float(
+        vec3(
+            (r + x) / 12.0,
+            (r + y) / 12.0,
+            0.0
+        ),
+        NoiseParams(7.0f, 10.0f, 0.2f, 0.0f, 0.0001, 0.13, 0.85)
+    );
+
+    prominences = smoothstep(bright.gamma, bright.gamma + bright.ray_brightness, prominences);
+    prominences = sqrt(prominences);
+
+    color += saturate(mix(
+        0.8f - vec3(prominences),
+        vec3(0.85f),
+        bright.spot_brightness - 3000.0 * r / (36.0f * bright.brightness)
+    ) * 25.0f * _color);
+    color += saturate(mix(
+        0.8f - vec3(prominences),
+        vec3(0.8f),
+        bright.spot_brightness - 3000.0 * r / (36.0f * bright.brightness)
+    ) * 7.5f * _color);
+
+    vec2 flares_dist = noise_fbm_vec2(
+        vec3(
+            (r + x) / 10.0f,
+            (r + y) / 10.0f,
+            0.0f
+        ),
+        NoiseParams(6.0f, 6.0f, 0.2f, 0.0f, 0.0001, 0.13, 0.85)
+    );
+
+    uv  = (in_tex_coord + dist * 0.01f + flares_dist * 0.01f) * 2.0f - 1.0f;
 	uvn = normalize(uv);
 
     r0 = sqrt(dot(uv0, uv0));
@@ -99,28 +157,10 @@ void main(void)
     x = dot(uvn, vec2(0.5, 0.0)) - eye.anim_time * 3.0f;
     y = dot(uvn, vec2(0.0, 0.5)) - eye.anim_time * 3.0f;
 
-    float surface = noise_fbm_float(
+    float flares = noise_fbm_float(
         vec3(
-            (r + x) / 60.0 * shape.ray_density,
-            (r + y) / 60.0 * shape.ray_density,
-            0.0
-        ),
-        NoiseParams(4.9f, 8.0f, 0.0f, 0.0f, 0.0001, 0.13, 0.85)
-    );
-
-    surface = smoothstep(bright.gamma, bright.gamma + bright.ray_brightness, surface);
-    surface = sqrt(surface);
-
-    color += saturate(mix(
-        1.0f - vec3(surface),
-        vec3(1.15f),
-        bright.spot_brightness - 3000.0 * r / (36.0f * bright.brightness)
-    ) * 10.8f * _color);
-
-    float prominences = noise_fbm_float(
-        vec3(
-            (r + x) / 60.0 * shape.ray_density,
-            (r + y) / 60.0 * shape.ray_density,
+            (r + x) / 10.0,
+            (r + y) / 10.0,
             0.0
         ),
         NoiseParams(3.2f, 8.0f, 0.4f, 0.0f, 0.0001, 0.1, 0.55)
@@ -130,37 +170,27 @@ void main(void)
         color.rgb *= 0.4f;
     }
 
-    // Doing this 3 times allows prominences to be brighter near the middle. There's probably a
-    // better way to do this, but I'm inexperienced.
-
     color += saturate(
         mix(
-            1.0f - vec3(prominences),
-            vec3(0.55f),
+            1.0f - vec3(flares),
+            vec3(0.7f),
             bright.spot_brightness - 2000.0 / (36.0f * bright.brightness)
         ) * _color
-    ) * (0.7f - min(eucl_dist, 0.5f));
+    ) * 3.5f / eucl_dist;
     color += saturate(
         mix(
-            1.0f - vec3(prominences),
-            vec3(0.6f),
+            1.0f - vec3(flares),
+            vec3(0.75f),
             bright.spot_brightness - 2000.0 / (36.0f * bright.brightness)
         ) * _color
-    ) * (4.0f - eucl_dist);
-    color += saturate(
-        mix(
-            1.0f - vec3(prominences),
-            vec3(0.65f),
-            bright.spot_brightness - 2000.0 / (36.0f * bright.brightness)
-        ) * _color
-    ) * (7.0f - eucl_dist);
+    ) * 7.0f / eucl_dist;
 
     vec3 ray = in_pix_pos + eye.pos;
-    float dist = length(ray);
+    float eye_dist = length(ray);
 
-    float fade = clamp(dist - 0.01f, 0.0f, 1.0f)
+    float fade = clamp(eye_dist - 0.05f, 0.0f, 1.0f)
         * clamp(1.0 - r0, 0.0, 1.0)
-        * clamp(abs(ray.z / dist) - 0.1, 0.0, 1.0);
+        * clamp(abs(ray.z / eye_dist) - 0.1, 0.0, 1.0);
 
     out_color.rgb = fade * pow(max(color.rgb, 0.0), vec3(2.2f));
 }
